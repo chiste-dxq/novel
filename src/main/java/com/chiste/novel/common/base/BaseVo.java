ec46e31fcd877fbe89ee4c52df4ba94c7dd117da
chiste-dxq <80678738+chiste-dxq@users.noreply.github.com>
2021-04-23 17:00:38 +0800

    基础类

--- src/main/java/com/chiste/novel/common/base/BaseVo.java
+++ src/main/java/com/chiste/novel/common/base/BaseVo.java
@@ -1,10 +1,19 @@
 package com.chiste.novel.common.base;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
 /*
  * @Author: daixq
  * @Date: 2021/4/21 17:10
  * @Description:
  **/
+@Data
 public class BaseVo {
+
+    @ApiModelProperty("每页数量")
+    private Integer pageSize;
+    @ApiModelProperty("页码")
+    private Integer page;
 
 }
