88ab516f6fcd45e445747429d51a71870682cf32
daixq <80678738+chiste-dxq@users.noreply.github.com>
2021-04-23 17:43:43 +0800

    工具类

---
+++ src/main/java/com/chiste/novel/service/novel/NovelCatService.java
@@ -0,0 +1,4 @@
+package com.chiste.novel.service.novel;
+
+public interface NovelCatService {
+}
---
+++ src/main/java/com/chiste/novel/service/novel/impl/NovelCatServiceImpl.java
@@ -0,0 +1,13 @@
+package com.chiste.novel.service.novel.impl;
+
+import com.chiste.novel.service.novel.NovelCatService;
+import org.springframework.stereotype.Service;
+
+/*
+ * @Author: daixq
+ * @Date: 2021/4/23 17:43
+ * @Description:
+ **/
+@Service
+public class NovelCatServiceImpl implements NovelCatService {
+}
